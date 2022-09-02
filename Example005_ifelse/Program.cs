a133c0ac25d0b194c6ac03bec9f4f2ec4f72c418
unknown <2296510@mail.ru>
2022-09-03 00:29:46 +0500

    1

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if(username == "Маша")
+{
+    Console.WriteLine ("Ура, это же Маша!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
+
