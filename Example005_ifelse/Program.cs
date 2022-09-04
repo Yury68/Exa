532a9b2b2fd12769659e49d36bd7f58cd2821cc2
unknown <2296510@mail.ru>
2022-09-04 18:59:10 +0500

    Ex006

--- Example005_ifelse/Program.cs
+++ Example005_ifelse/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите имя пользователя ");
 string username = Console.ReadLine();
-if(username.ToLower() == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine ("Ура, это же Маша!");
 }
